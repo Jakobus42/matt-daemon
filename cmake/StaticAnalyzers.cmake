cef8a1040c45909625c65395d328400e946f1d5f
Jakobus42 <jakob.sadjina@icloud.com>
2026-01-25 03:54:52 +0100

    build: fix clang-tidy fails on unkown warning

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -2,8 +2,8 @@ function(matt_daemon_enable_static_analyses ENABLE_CLANG_TIDY)
   if(${ENABLE_CLANG_TIDY})
     find_program(CLANGTIDY clang-tidy)
     if(CLANGTIDY)
-      set(MATT_DAEMON_CLANG_TIDY
-          ${CLANGTIDY}
+      set(MATT_DAEMON_CLANG_TIDY_OPTIONS
+          ${CLANGTIDY} -extra-arg=-Wno-unknown-warning-option
           CACHE INTERNAL "clang-tidy exec path")
       message(STATUS "clang-tidy enabled: ${CLANGTIDY}")
     else()
@@ -17,7 +17,7 @@ endfunction()
 # dependencies first and then set global clang tidy op?
 function(matt_daemon_apply_static_analyses TARGET)
   if(MATT_DAEMON_CLANG_TIDY)
-    set_target_properties(${TARGET} PROPERTIES CXX_CLANG_TIDY
-                                               "${MATT_DAEMON_CLANG_TIDY}")
+    set_target_properties(
+      ${TARGET} PROPERTIES CXX_CLANG_TIDY "${MATT_DAEMON_CLANG_TIDY_OPTIONS}")
   endif()
 endfunction()
