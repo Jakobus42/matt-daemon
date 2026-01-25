033c05462c7ae550022b333783bf5076d62ff6dd
Jakobus42 <jakob.sadjina@icloud.com>
2026-01-25 23:53:08 +0100

    ci: setup release pipeline

--- matt-daemon-rpc/include/matt-daemon-rpc/foo.hpp
+++ matt-daemon-rpc/include/matt-daemon-rpc/foo.hpp
@@ -1,9 +1,12 @@
 #ifndef MATT_DAEMON_RPC_FOO
 #define MATT_DAEMON_RPC_FOO
 
+#include <print>
+
 namespace matt_daemon_rpc {
 
 inline auto foo() -> int {
+  std::print("Hello World!\n");
   return 1;
 }
 
