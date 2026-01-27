eafc1278886c0a82fda52cf05c5dd82fc5d10abb
Jakobus42 <jakob.sadjina@icloud.com>
2026-01-27 20:48:11 +0100

    build: setup clang-p2996

--- matt-daemon/src/main.cpp
+++ matt-daemon/src/main.cpp
@@ -1,5 +1,27 @@
-#include <matt-daemon-rpc/foo.hpp>
+// from: https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2025/p2996r13.html#enum-to-string
 
-auto main() -> int {
-  return matt_daemon_rpc::foo();
+#include <meta>
+#include <print>
+
+template<typename E, bool Enumerable = std::meta::is_enumerable_type(^^E)>
+  requires std::is_enum_v<E>
+constexpr std::string_view enum_to_string(E value) {
+  if constexpr (Enumerable)
+    template for (constexpr auto e :
+                  std::define_static_array(std::meta::enumerators_of(^^E)))
+      if (value == [:e:])
+        return std::meta::identifier_of(e);
+
+  return "<unnamed>";
+}
+
+int main() {
+  enum Color : int;
+  static_assert(enum_to_string(Color(0)) == "<unnamed>");
+  std::println("Color 0: {}", enum_to_string(Color(0)));  // prints '<unnamed>'
+
+  enum Color : int { red, green, blue };
+  static_assert(enum_to_string(Color::red) == "red");
+  static_assert(enum_to_string(Color(42)) == "<unnamed>");
+  std::println("Color 0: {}", enum_to_string(Color(0)));  // prints 'red'
 }
