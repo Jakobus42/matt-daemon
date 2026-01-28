8ec920a17ecdfd62740cbec56769a7ad7e187763
Jakobus42 <jakob.sadjina@icloud.com>
2026-01-28 22:01:55 +0000

    feat: reflection example

--- matt-daemon/src/main.cpp
+++ matt-daemon/src/main.cpp
@@ -1,27 +1,37 @@
-// from: https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2025/p2996r13.html#enum-to-string
+// from:
+// https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2025/p2996r13.html#enum-to-string
 
+#include <cstdint>
 #include <meta>
 #include <print>
 
-template<typename E, bool Enumerable = std::meta::is_enumerable_type(^^E)>
-  requires std::is_enum_v<E>
-constexpr std::string_view enum_to_string(E value) {
-  if constexpr (Enumerable)
-    template for (constexpr auto e :
-                  std::define_static_array(std::meta::enumerators_of(^^E)))
-      if (value == [:e:])
-        return std::meta::identifier_of(e);
+namespace {
 
+template <typename E, bool Enumerable = std::meta::is_enumerable_type(^^E)>
+  requires std::is_enum_v<E>
+constexpr auto enum_to_string(E value) -> std::string_view {
+  if constexpr (Enumerable) {
+    // NOLINTBEGIN - false positive
+    template for (constexpr auto elem :
+                  std::define_static_array(std::meta::enumerators_of(^^E))) {
+    // NOLINTEND
+      if (value == [:elem:]) {
+        return std::meta::identifier_of(elem);
+      }
+    }
+  }
   return "<unnamed>";
 }
 
-int main() {
-  enum Color : int;
+}  // namespace
+
+auto main() -> int {
+  enum Color : std::uint8_t;
   static_assert(enum_to_string(Color(0)) == "<unnamed>");
   std::println("Color 0: {}", enum_to_string(Color(0)));  // prints '<unnamed>'
 
-  enum Color : int { red, green, blue };
+  enum Color : std::uint8_t { red, green, blue };
   static_assert(enum_to_string(Color::red) == "red");
-  static_assert(enum_to_string(Color(42)) == "<unnamed>");
+  static_assert(enum_to_string(Color(3)) == "<unnamed>");
   std::println("Color 0: {}", enum_to_string(Color(0)));  // prints 'red'
 }
