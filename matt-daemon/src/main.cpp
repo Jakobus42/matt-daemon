fcca28f0c4b726f67283ca45f76274b369e9a0bf
Jakob <144040233+Jakobus42@users.noreply.github.com>
2026-01-29 22:12:54 +0100

    build: setup clang-p2996 (#9)
    * build: setup clang-p2996
    
    * build: use forked clang-p2996 image
    
    * feat: add devcontainer for helix
    
    * ci: took the L and just removed asan and tsan from ci
    
    * feat: reflection example
    
    * ci: request format with comment action
    
    * ci: forgot permissions
    
    ---------
    
    Co-authored-by: Simon Dablander <ifaoji@pm.me>

--- matt-daemon/src/main.cpp
+++ matt-daemon/src/main.cpp
@@ -1,9 +1,37 @@
-#include <matt-daemon-interface/bar/bar.hpp>
-#include <matt-daemon-interface/foo/foo.hpp>
-#include <matt-daemon-rpc/foo.hpp>
+// from:
+// https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2025/p2996r13.html#enum-to-string
+
+#include <cstdint>
+#include <meta>
+#include <print>
+
+namespace {
+
+template <typename E, bool Enumerable = std::meta::is_enumerable_type(^^E)>
+  requires std::is_enum_v<E>
+constexpr auto enum_to_string(E value) -> std::string_view {
+  if constexpr (Enumerable) {
+    // NOLINTBEGIN - false positive
+    template for (constexpr auto elem :
+                  std::define_static_array(std::meta::enumerators_of(^^E))) {
+    // NOLINTEND
+      if (value == [:elem:]) {
+        return std::meta::identifier_of(elem);
+      }
+    }
+  }
+  return "<unnamed>";
+}
+
+}  // namespace
 
 auto main() -> int {
-  matt_daemon_rpc::foo();
-  matt_daemon_interface::bar::bar();
-  matt_daemon_interface::foo::foo();
+  enum Color : std::uint8_t;
+  static_assert(enum_to_string(Color(0)) == "<unnamed>");
+  std::println("Color 0: {}", enum_to_string(Color(0)));  // prints '<unnamed>'
+
+  enum Color : std::uint8_t { red, green, blue };
+  static_assert(enum_to_string(Color::red) == "red");
+  static_assert(enum_to_string(Color(3)) == "<unnamed>");
+  std::println("Color 0: {}", enum_to_string(Color(0)));  // prints 'red'
 }
